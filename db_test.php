bbc983ede8f2ed86943f3efa951c8c4e55d250c8
morita <moritat1976@gmail.com>
2014-05-01 22:43:29 +0900

    Modified: mysql_connect() function -> mysqli_connect()

--- db_test.php
+++ db_test.php
@@ -1,7 +1,7 @@
 <?php
-  $link = mysql_connect('127.0.0.1', 'root', '');
+  $link = mysqli_connect('127.0.0.1', 'root', '');
   if (!$link) {
-    die('接続失敗！<br>'.mysql_error());
+    die('接続失敗！<br>'.mysqli_connect_error());
   } else {
         echo '接続成功！';
   }
